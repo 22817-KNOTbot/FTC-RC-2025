7b0058364b59890d3a3d6e8721178608fd2cfe12
hotwh-et <ethan10.wong@gmail.com>
2024-09-30 10:16:44 -0700

    Added MeepMeep with draft path

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,53 @@
+package com.example.meepmeeptesting;
+
+// import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(62, -62, Math.toRadians(90)))
+                .splineToSplineHeading(new Pose2d(7, -35, Math.toRadians(270)), Math.toRadians(90))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(23, -40, Math.toRadians(0)), Math.toRadians(0))
+
+                .splineToLinearHeading(new Pose2d(39, -25, Math.toRadians(0)), Math.toRadians(350))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(30, -40, Math.toRadians(0)), Math.toRadians(225))
+                .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(225)), Math.toRadians(180))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(-45, -45, Math.toRadians(0)), Math.toRadians(0))
+                
+                .splineToSplineHeading(new Pose2d(49, -25, Math.toRadians(0)), Math.toRadians(25))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(40, -40, Math.toRadians(245)), Math.toRadians(205))
+                .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(225)), Math.toRadians(180))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(-45, -45, Math.toRadians(0)), Math.toRadians(0))
+                
+                .splineToSplineHeading(new Pose2d(59, -25, Math.toRadians(0)), Math.toRadians(25))
+                .waitSeconds(1)
+                .splineToSplineHeading(new Pose2d(40, -30, Math.toRadians(180)), Math.toRadians(205))
+                .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(225)), Math.toRadians(180))
+                .waitSeconds(1)
+
+                .turn(Math.toRadians(-135))
+                .splineToLinearHeading(new Pose2d(-25, -15, Math.toRadians(180)), Math.toRadians(0))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
