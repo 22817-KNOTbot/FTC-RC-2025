b97c774a2073faf11717c95b0c87b37907659675
hotwh-et <ethan10.wong@gmail.com>
2025-01-06 19:32:37 -0800

    Fix: Change touch sensor testing OpMode to only use 1 type of sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TouchSensorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TouchSensorTesting.java
@@ -15,18 +15,34 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @TeleOp(name="Touch sensor testing", group="Debug")
 public class TouchSensorTesting extends LinearOpMode {
 	public static String TOUCH_SENSOR_NAME = "intakeTouch";
+	public static String DIGITAL_CHANNEL_NAME = "intakeTouch";
+	public static Sensors SENSOR = Sensors.TOUCH_SENSOR;
+
+	public enum Sensors {
+		TOUCH_SENSOR,
+		DIGITAL_CHANNEL
+	}
 
 	@Override
 	public void runOpMode() {
 		telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-		TouchSensor touch = hardwareMap.get(TouchSensor.class, TOUCH_SENSOR_NAME);
-		DigitalChannel digital = hardwareMap.get(DigitalChannel.class, TOUCH_SENSOR_NAME);
+		TouchSensor touch = null;
+		DigitalChannel digital = null;
+
+		switch (SENSOR) {
+			case TOUCH_SENSOR:
+				touch = hardwareMap.get(TouchSensor.class, TOUCH_SENSOR_NAME);
+				break;
+			case DIGITAL_CHANNEL:
+				digital = hardwareMap.get(DigitalChannel.class, TOUCH_SENSOR_NAME);
+				break;
+		}
 		waitForStart();
 
 		while (opModeIsActive()) {
-			telemetry.addData("Touch", touch.isPressed());
-			telemetry.addData("Digital", !digital.getState());
+			telemetry.addData("Touch", (touch != null) ? touch.isPressed() : "None");
+			telemetry.addData("Digital", (digital != null) ? !digital.getState() : "None");
 			telemetry.update();
 
 			sleep(50);
