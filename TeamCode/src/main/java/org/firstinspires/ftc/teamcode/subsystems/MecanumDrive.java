6a3ca956d22bd406bb1529ef075fcb367ae9df62
aloalo151 <nguyen.ddk274@gmail.com>
2025-09-08 15:54:32 -0700

    Finished making Constructor, drive, and lockingMecanum functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDrive.java
@@ -1,44 +1,64 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+
+
 
 
 public class MecanumDrive {
 
-	public void runOpMode(){
 
-		waitForStart();
+	private Pose holdPose = new Pose(0, 0, 0);
+	private boolean holdingPose;
+    private Follower follower;
+
+    public MecanumDrive(HardwareMap hardwareMap){
+        follower = Constants.createFollower(hardwareMap);
+    }
+
+	public void mecanumDrive(float forward, float lateral, float rotation){
+		
 		follower.startTeleopDrive();
 
-		while (opModeIsActive){
-			follower.update();
+        follower.update();
 
-			double forward = -gamepad1.left_stick_y;
-			double lateral = -gamepad1.left_stick_x;
-			double rotation = -gamepad1.right_stick_x;
+        double denominator = Math.max(Math.abs(forward) + Math.abs(lateral) + Math.abs(rotation), 1);
 
-			double denominator = Math.max(Math.abs(forward) + Math.abs(lateral) + Math.abs(rotation), 1);
+        follower.setTeleOpDrive(
+            forward / denominator,
+            lateral / denominator,
+            rotation / denominator,
+            false); 
 
-			follower.setTeleOpMovementVectors(
-				forward / denominator,
-				lateral / denominator,
-				rotation / denominator,
-				false); 
-
-			if (!holdingPose && gamepad1.left_bumper) {
-				holdPose = follower.getPose();
-				follower.holdPoint(holdPose);
-				holdingPose = true;
-			} else if (holdingPose && !gamepad1.left_bumper) {
-				follower.breakFollowing();
-				follower.startTeleopDrive();
-				holdingPose = false;
-			}
-		}
+        // if (!holdingPose && gamepad1.left_bumper) {
+        //     holdPose = follower.getPose();
+        //     follower.holdPoint(holdPose);
+        //     holdingPose = true;
+        // } else if (holdingPose && !gamepad1.left_bumper) {
+        //     follower.breakFollowing();
+        //     follower.startTeleopDrive();
+        //     holdingPose = false;
+        // }
 
 	}
+
+    public void lockingMecanum(boolean enabled){
+        if (enabled){
+            holdPose = follower.getPose();
+             follower.holdPoint(holdPose);
+             holdingPose = true;
+        }else{
+            follower.breakFollowing();
+             follower.startTeleopDrive();
+             holdingPose = false;
+        }
+    }
 
 }
 
