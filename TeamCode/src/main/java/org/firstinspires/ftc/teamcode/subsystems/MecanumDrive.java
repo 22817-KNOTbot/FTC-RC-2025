d3084f0a762973f5a3dbe461faba37d79c01ad34
aloalo151 <nguyen.ddk274@gmail.com>
2025-09-09 16:47:20 -0700

    Removed whitespace, fixed indenting, fixed lockingMecanum, renamed mecanumDrive to mecanumDriveMove, and made mecanumDriveInitialize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDrive.java
@@ -8,50 +8,39 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 
-
-
-
 public class MecanumDrive {
-
 
 	private Pose holdPose = new Pose(0, 0, 0);
 	private boolean holdingPose;
-    private Follower follower;
+	private Follower follower;
 
-    public MecanumDrive(HardwareMap hardwareMap){
-        follower = Constants.createFollower(hardwareMap);
-    }
-
-	public void mecanumDrive(float forward, float lateral, float rotation){
-		
-		follower.startTeleopDrive();
-
-        follower.update();
-
-        double denominator = Math.max(Math.abs(forward) + Math.abs(lateral) + Math.abs(rotation), 1);
-
-        follower.setTeleOpDrive(
-            forward / denominator,
-            lateral / denominator,
-            rotation / denominator,
-            false); 
-
-        
-
+	public MecanumDrive(HardwareMap hardwareMap) {
+		follower = Constants.createFollower(hardwareMap);
 	}
 
-    public void lockingMecanum(boolean enabled){
-        if (enabled){
-            holdPose = follower.getPose();
-             follower.holdPoint(holdPose);
-             holdingPose = true;
-        }else{
-            follower.breakFollowing();
-             follower.startTeleopDrive();
-             holdingPose = false;
-        }
-    }
+	public void mecanumDriveInitialize() {
+		follower.startTeleopDrive();
+	}
 
+	public void mecanumDriveMove(float forward, float lateral, float rotation) {
+		follower.update();
+		double denominator = Math.max(Math.abs(forward) + Math.abs(lateral) + Math.abs(rotation), 1);
+		follower.setTeleOpDrive(
+			forward / denominator,
+			-lateral / denominator,
+			-rotation / denominator,
+			false);
+	}
+
+	public void lockingMecanum(boolean enabled) {
+		if (enabled && !holdingPose) {
+			holdPose = follower.getPose();
+			follower.holdPoint(holdPose);
+			holdingPose = true;
+		} else if (!enabled && holdingPose) {
+			follower.breakFollowing();
+			follower.startTeleopDrive();
+			holdingPose = false;
+		}
+	}
 }
-
-
