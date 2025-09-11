d326f6a59a65e857529efeaf2e7176a2c84177b6
aloalo151 <nguyen.ddk274@gmail.com>
2025-09-11 13:56:07 -0700

    Added constructor
    changed setPower
    
    Added configurable variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,22 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.bylazar.configurables.annotations.Configurable;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Configurable
 public class Intake {
+	public static boolean on = false;
+	public static float power = 1;
 
-    private boolean on = false;
-    private float power = 0;
+	private DcMotor intake;
 
-    public void enable(boolean enable) {
-        if (!on && enable){
-            intake.setPower(power);
-        } else if (on && !enable) {
-            intake.setPower(0);
-        }
-    }
+	public Intake(HardwareMap hardwareMap) {
+		intake = hardwareMap.get(DcMotor.class, "intake");
+		intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+	}
 
-    public void setPower(float pow) {
-        power = pow;
-    }
+	public void enable(boolean enable) {
+		if (!on && enable) {
+			intake.setPower(power);
+		} else if (on && !enable) {
+			intake.setPower(0);
+		}
+	}
 
+	public void setPower(float pow) {
+		intake.setPower(pow);
+	}
 }
