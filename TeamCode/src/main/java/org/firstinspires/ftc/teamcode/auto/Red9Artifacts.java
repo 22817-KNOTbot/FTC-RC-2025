bd4453bb89302d58dc13bd5b96e018e844b5585e
Dten4321 <shalena.ca@gmail.com>
2025-09-18 19:35:46 -0700

    Added actions to red9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Red9Artifacts.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Red9Artifacts.java
@@ -7,6 +7,10 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.scoring.Artifact;
+
+import org.firstinspires.ftc.teamcode.teleop.Automations;
+import org.firstinspires.ftc.teamcode.util.Alliance;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
@@ -20,10 +24,18 @@ public class Red9Artifacts extends LinearOpMode {
 	public static boolean doMovement = true;
 	public static boolean doActions = true;
 
+	private Automations automationsHandler;
+	private Alliance alliance;
+	private HardwareMap hardwareMap;
+
 	private int pathState = 0;
+	private int autoState = 0;
+	private boolean DEBUG;
 
 	private Follower follower;
 	private Pose startPose;
+
+	private Artifact.Colour[] pattern;
 
 	private PathChain firstApproach, firstIntake, firstLaunch,
 			secondIntake, secondLaunch;
@@ -34,11 +46,21 @@ public class Red9Artifacts extends LinearOpMode {
 		follower.setStartingPose(startPose);
 		buildPaths();
 
+		automationsHandler = new Automations(hardwareMap, alliance, DEBUG);
+
+
 		waitForStart();
 
 		while (opModeIsActive()) {
+			follower.update();
 			if (doMovement) {
 				pathUpdate();
+				automationsHandler.updatePose(follower.getPose());
+			}
+
+			if (doActions) {
+				automationsHandler.automationLoop();
+				actionsDo();
 			}
 		}
 	}
@@ -91,39 +113,80 @@ public class Red9Artifacts extends LinearOpMode {
 	public void setPathState(int state) {
 		pathState = state;
 	}
+	public void setActionState(int state) {
+		autoState = state;
+	}
 
 	public void pathUpdate() {
 		switch (pathState) {
 			case 0:
 				if (!follower.isBusy()) {
+					actionsDo();
 					follower.followPath(firstApproach, true);
+					setActionState(1);
 					setPathState(1);
 				}
 				break;
 			case 1:
 				if (!follower.isBusy()) {
 					follower.followPath(firstIntake, true);
+					actionsDo();
+					setActionState(2);
 					setPathState(2);
 				}
 				break;
 			case 2:
 				if (!follower.isBusy()) {
 					follower.followPath(firstLaunch, true);
+					actionsDo();
+					setActionState(3);
 					setPathState(3);
 				}
 				break;
 			case 3:
 				if (!follower.isBusy()) {
 					follower.followPath(secondIntake, true);
+					actionsDo();
+					setActionState(4);
 					setPathState(4);
 				}
 				break;
 			case 4:
 				if (!follower.isBusy()) {
 					follower.followPath(secondLaunch, true);
+					actionsDo();
+					setActionState(-1);
 					setPathState(-1);
 				}
 				break;
+		}
+	}
+	public void actionsDo() {
+		if (doActions) {		
+			switch (autoState) {
+				case 0:
+					break;
+				case 1:
+					automationsHandler.intakeToggle();
+					break;
+				case 2:
+					automationsHandler.intakeToggle();
+					pattern = automationsHandler.getPatternShort().getPattern();
+					automationsHandler.shootArtifact(pattern[0]);
+					automationsHandler.shootArtifact(pattern[1]);
+					automationsHandler.shootArtifact(pattern[2]);
+					break;
+				case 3:
+					automationsHandler.intakeToggle();
+					break;
+				case 4:
+					pattern = automationsHandler.getPatternShort().getPattern();
+					automationsHandler.shootArtifact(pattern[0]);
+					automationsHandler.shootArtifact(pattern[1]);
+					automationsHandler.shootArtifact(pattern[2]);
+					break;
+
+			}
 		}
 	}
 }
