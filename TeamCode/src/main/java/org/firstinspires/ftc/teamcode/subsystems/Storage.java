2bee58b5d197c74f26c5fc76aee503c32d5665d8
Dten4321 <shalena.ca@gmail.com>
2025-09-24 21:14:53 -0700

    Changed the storage to match new system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -12,62 +13,26 @@ import org.firstinspires.ftc.teamcode.scoring.Artifact.Colour;
 
 @Configurable
 public class Storage {
+
+	private DcMotor storageMotor = hardwareMap.get(DcMotor.class, "storageMotor");
+
 	public static double distance_threshold_mm = 5;
+	public static int motorPos = 1;
+	public static double positionGap = 5; //arbitary mumber, will change with further testing
+	public static Colour artifactStored[] = {null, null, null};
 
-	private static Colour frontLeftArtifact = null;
-	private static Colour frontRightArtifact = null;
-	private static Colour backArtifact = null;
+	private static int numOfArtifacts = 0;
 
-	private Gate frontLeftGate;
-	private Gate frontRightGate;
-	private Gate backLeftGate;
-	private Gate backRightGate;
 	private ColorRangeSensor colourSensor;
 
 	private boolean previouslyLoaded;
 
 	public Storage(HardwareMap hardwareMap) {
-		frontLeftGate = new Gate(hardwareMap.get(Servo.class, "frontLeftGateServo"));
-		frontRightGate = new Gate(hardwareMap.get(Servo.class, "frontRightGateServo"));
-		backLeftGate = new Gate(hardwareMap.get(Servo.class, "backLeftGateServo"));
-		backRightGate = new Gate(hardwareMap.get(Servo.class, "backRightGateServo"));
 
 		colourSensor = hardwareMap.get(ColorRangeSensor.class, "colourSensor");
 	}
 
-	public enum Chamber {
-		LEFT,
-		RIGHT
-	}
-
 	@Configurable
-	public static class Gate {
-		public static double gate_open = 0;
-		public static double gate_closed = 0;
-
-		private Servo servo;
-		private boolean open = false;
-
-		protected Gate(Servo servo) {
-			this.servo = servo;
-			open = servo.getPosition() == gate_open;
-		}
-
-		public void open() {
-			servo.setPosition(gate_open);
-			open = true;
-		}
-
-		public void close() {
-			servo.setPosition(gate_closed);
-			open = false;
-		}
-
-		public boolean isOpen() {
-			return open;
-		}
-	}
-
 	public void abort() {
 		// Currently does nothing
 		// Exists for future use
@@ -76,84 +41,56 @@ public class Storage {
 	/*
 	 * Getter methods
 	 */
-
-	public static Colour getFrontLeftArtifact() {
-		return frontLeftArtifact;
+	
+	public static Colour getFirstArtifact() {
+		return artifactStored[0];
+	}
+	public static Colour getSecondArtifact() {
+		return artifactStored[1];
+	}
+	public static Colour getThirdArtifact() {
+		return artifactStored[2];
 	}
 
-	public static Colour getFrontRightArtifact() {
-		return frontRightArtifact;
-	}
-
-	public static Colour getBackArtifact() {
-		return backArtifact;
-	}
-
-	public Gate getFrontLeftGate() {
-		return frontLeftGate;
-	}
-
-	public Gate getFrontRightGate() {
-		return frontRightGate;
-	}
-
-	public Gate getBackLeftGate() {
-		return backLeftGate;
-	}
-
-	public Gate getBackRightGate() {
-		return backRightGate;
-	}
-
+	
 	/*
 	 * Storage
 	 */
 
 	public boolean intake() {
-		backLeftGate.close();
-		backRightGate.close();
 		if (isArtifactLoaded()) {
-			backArtifact = getArtifactColour();
+			artifactStored.set(numOfArtifacts, getArtifactColour());
+			numOfArtifacts += 1;
+			storageMotor.setTargetPosition(getCurrentPosition() + positionGap);
 			return true;
 		}
 		return false;
 	}
 
-	// Returns the chamber if successfully stored
-	// Returns null if both slots are full
-	public Chamber storeArtifact() {
-		if (frontLeftArtifact == null) {
-			backLeftGate.open();
-			frontLeftGate.close();
-			frontLeftArtifact = backArtifact;
-			backArtifact = null;
-			return Chamber.LEFT;
-		} else if (frontRightArtifact == null) {
-			backRightGate.open();
-			frontRightGate.close();
-			frontRightArtifact = backArtifact;
-			backArtifact = null;
-			return Chamber.RIGHT;
+	// Returns the storage if successfully stored
+	// Returns null if storage is full
+	public storageFull() {
+		if (artifactStored.contains(null)) {
+			return;
+		} else {
+			return null;
 		}
-		return null;
 	}
 
-	public boolean releaseLeft() {
-		if (frontLeftArtifact == null)
-			return false;
-		
-		frontLeftGate.open();
-		frontLeftArtifact = null;
-		return true;
-	}
 
-	public boolean releaseRight() {
-		if (frontRightArtifact == null)
-			return false;
-		
-		frontRightGate.open();
-		frontRightArtifact = null;
-		return true;
+	public release() {
+		if (getThirdArtifact() != null) {
+			artifactStored.set(numOfArtifacts, null);
+			numOfArtifacts -= 1;
+		} else if (getSecondArtifact() != null) {
+			artifactStored.set(numOfArtifacts, null);
+			numOfArtifacts -= 1;
+		} else if (getFirstArtifact() != null) {
+			artifactStored.set(numOfArtifacts, null);
+			numOfArtifacts -= 1;
+		} else {
+			return null;
+		}
 	}
 
 	/*
