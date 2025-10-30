8037d46a2601c9aa842426b2ee772a3b6060e579
hotwh-et <ethan10.wong@gmail.com>
2025-10-30 12:00:50 -0700

    Replace Dashbaord with Slothboard. Add FTC Practice App. Update Turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TurretTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TurretTesting.java
@@ -4,6 +4,8 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.JoinedTelemetry;
 import com.bylazar.telemetry.PanelsTelemetry;
 
+import com.acmerobotics.dashboard.config.Config;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -11,6 +13,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.Turret;
 
 @Configurable
+@Config
 // @TeleOp(name="Turret testing", group="Debug")
 public class TurretTesting extends LinearOpMode {
 	public static Mode mode = Mode.ABSOLUTE;
