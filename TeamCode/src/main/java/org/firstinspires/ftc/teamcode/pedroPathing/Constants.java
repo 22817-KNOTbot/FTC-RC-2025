5bf40f928803f1d380403cadd9707cfc2817ffdb
hotwh-et <ethan10.wong@gmail.com>
2025-11-24 19:31:21 -0800

    Make localizer constants configurable for testing purposes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,24 +1,33 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
-import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.paths.PathConstraints;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Config
 public class Constants {
-	public static FollowerConstants followerConstants = new FollowerConstants()
+	public static double forwardTicksToInches = 0.001997263346781042;
+	public static double strafeTicksToInches = 0.00201150257521259037;
+	public static double turnTicksToInches = 0.0020001;
+	public static double leftPodY = 7.113188976;
+	public static double rightPodY = -7.113188976;
+	public static double strafePodX = -6.5;
+
+	public final static FollowerConstants followerConstants = new FollowerConstants()
 			.mass(14.5);
 
-	public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+	public final static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
-	public static MecanumConstants driveConstants = new MecanumConstants()
+	public final static MecanumConstants driveConstants = new MecanumConstants()
 			.maxPower(1)
 			.leftFrontMotorName("frontLeftMotor")
 			.rightFrontMotorName("frontRightMotor")
@@ -30,29 +39,44 @@ public class Constants {
 			.rightRearMotorDirection(DcMotor.Direction.FORWARD)
 			.useBrakeModeInTeleOp(true);
 
-	public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-			.forwardTicksToInches(0.0019859077)
-			.strafeTicksToInches(0.0019831188)
-			.turnTicksToInches(0.0019991271)
-			.leftPodY(7.286)
-			.rightPodY(-7.144)
-			.strafePodX(-7.042)
+	public final static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+			.forwardTicksToInches(0.001987263346781042)
+			.strafeTicksToInches(0.0019750257521259037)
+			.turnTicksToInches(0.0019990158631831103)
+			.leftPodY(7.113188976)
+			.rightPodY(-7.113188976)
+			.strafePodX(-7.01423031496)
 			.leftEncoder_HardwareMapName("frontLeftMotor")
 			.rightEncoder_HardwareMapName("shooterMotorRight")
 			.strafeEncoder_HardwareMapName("backLeftMotor")
 			.leftEncoderDirection(Encoder.REVERSE)
 			.rightEncoderDirection(Encoder.REVERSE)
-			.strafeEncoderDirection(Encoder.FORWARD)
-			.IMU_HardwareMapName("imu")
-			.IMU_Orientation(new RevHubOrientationOnRobot(
-					RevHubOrientationOnRobot.LogoFacingDirection.LEFT, 
-					RevHubOrientationOnRobot.UsbFacingDirection.UP));
+			.strafeEncoderDirection(Encoder.FORWARD);
+			// .IMU_HardwareMapName("imu")
+			// .IMU_Orientation(new RevHubOrientationOnRobot(
+			// 		RevHubOrientationOnRobot.LogoFacingDirection.LEFT, 
+			// 		RevHubOrientationOnRobot.UsbFacingDirection.UP));
 
 	public static Follower createFollower(HardwareMap hardwareMap) {
 		return new FollowerBuilder(followerConstants, hardwareMap)
 				.pathConstraints(pathConstraints)
 				.mecanumDrivetrain(driveConstants)
-				.threeWheelIMULocalizer(localizerConstants)
+				.threeWheelLocalizer(
+						new ThreeWheelConstants()
+								.forwardTicksToInches(forwardTicksToInches)
+								.strafeTicksToInches(strafeTicksToInches)
+								.turnTicksToInches(turnTicksToInches)
+								.leftPodY(leftPodY)
+								.rightPodY(rightPodY)
+								.strafePodX(strafePodX)
+								// .strafePodX(-7.01423031496)
+								.leftEncoder_HardwareMapName("frontLeftMotor")
+								.rightEncoder_HardwareMapName("shooterMotorRight")
+								.strafeEncoder_HardwareMapName("backLeftMotor")
+								.leftEncoderDirection(Encoder.REVERSE)
+								.rightEncoderDirection(Encoder.REVERSE)
+								.strafeEncoderDirection(Encoder.FORWARD)
+				)
 				.build();
 	}
 }
