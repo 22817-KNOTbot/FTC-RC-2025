efacf19eaa085fb395b7b5f72277d7d5b648e05d
aloalo151 <nguyen.ddk274@gmail.com>
2026-02-10 15:12:42 -0800

    changed turnToArtifactSequence
    now finds the most scoring combination

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
@@ -140,9 +140,7 @@ public class Storage {
 		return numOfArtifacts;
 	}
 
-
-
-	public static int getNumbOfGreen() {
+	public static int getNumberOfGreen() {
 		int numOfGreenArtifacts = 0;
 		Iterator<Colour> artifactIterator = artifactStored.iterator(); 
 		while (artifactIterator.hasNext()) {
@@ -325,43 +323,61 @@ public class Storage {
 	// Will return NONE if invalid input or sequence not possible
 	// Only works for sequences of 3
 	public TurnDirection turnToArtifactSequence(Colour[] desiredSequence, boolean exactSequence) {
-		if (desiredSequence.length < 3 || !storageFull()) {
-			return TurnDirection.NONE;
+		getNumOfArtifacts();
+		if (!exactSequence) {
+			if (numOfArtifacts == 1) {
+				return turnToAnyArtifact();
+			}
+			int numOfGreen = getNumberOfGreen();
+			if (numOfGreen == 3 || numOfGreen== 0) {
+				return TurnDirection.AVAILABLE;
+			} 
 		}
 
-		if (!exactSequence) {
-			int numOfGreen = getNumbOfGreen();
-			if (numOfGreen > 2 || numOfGreen < 1) {
-				return TurnDirection.NONE;
-			} else if (numOfGreen == 2) {
-				if (
-				artifactStored.get(0) != desiredSequence[0]
-				&& artifactStored.get(1) != desiredSequence[1]
-				&& artifactStored.get(2) != desiredSequence[2]
-				) {
-					storageTurnCW();
-					return TurnDirection.CW;
+		if (numOfArtifacts == 3 || !exactSequence) {
+			int highestScore = 0;
+			int highestPos = -1;
+			for (int i = 0; i < 3; i++) {
+
+				int startingIndex = (3 - i) % 3;
+				List<Colour> sequence = new ArrayList<>();
+				sequence.add(artifactStored.get(startingIndex));
+				sequence.add(artifactStored.get((startingIndex + 1) % 3));
+				sequence.add(artifactStored.get((startingIndex + 2) % 3));
+
+				for (int x = 0; x < 3; x++) {
+					if (sequence.get(x) == null) {
+						sequence.remove(x);
+						sequence.add(null);
+					}
+				}
+
+				int currentScore = 0;
+				for (int x = 0; x < 3; x++) {
+					if (sequence.get(x) == desiredSequence[x]) {
+						currentScore += 2;
+					}
+				}
+				if (currentScore == 6) {
+					highestPos = i;
+					break;
+				}
+				if (currentScore > highestScore) {
+					highestScore = currentScore;
+					highestPos = i;
 				}
 			}
-		}
 
-		for (int i = 0; i < 3; i++) {
-			int startingIndex = (3 - i) % 3;
-			if (
-				artifactStored.get(startingIndex) == desiredSequence[0]
-				&& artifactStored.get((startingIndex + 1) % 3) == desiredSequence[1]
-				&& artifactStored.get((startingIndex + 2) % 3) == desiredSequence[2]
-			) {
-				switch (i) {
-					case 0:
-						return TurnDirection.AVAILABLE;
-					case 1:
-						storageTurnCW();
-						return TurnDirection.CW;
-					case 2:
-						storageDoubleTurnCW();
-						return TurnDirection.CCW;
-				}
+			switch (highestPos) {
+				case 0:
+					return TurnDirection.AVAILABLE;
+				case 1:
+					storageTurnCW();
+					return TurnDirection.CW;
+				case 2:
+					storageDoubleTurnCW();
+					return TurnDirection.CCW;
+				default:
 			}
 		}
 
